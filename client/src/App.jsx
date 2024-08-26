83fe04c606441bb4b0f87cbba66b067f4bfb856b
pjthakur <priyanshjadon@gmail.com>
2024-08-26 18:06:33 +0530

    created header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import Header from './components/Header'
 const App = () => {
   return (
     <div className='App'>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
